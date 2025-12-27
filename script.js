5e7bbed389c59b395706fc2880300755ae6402c3
mostafiz-devOps <mostafizurofficial@outlook.com>
2025-12-27 22:00:48 +0600

    update

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("ReTro-Forum script loaded.");
+const postContainer = document.getElementById("post-container");
+console.log(postContainer);
+const loadPosts = () => {
+  fetch("https://openapi.programming-hero.com/api/retro-forum/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      const forumLists = data.posts;
+      forumLists.forEach((forumList) => {
+        console.log(forumList);
+        const postDiv = document.createElement("div");
+        postDiv.innerHTML =`
+        
+        `;
+      });
+    });
+};
+
+loadPosts();
